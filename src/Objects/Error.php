d99a3ac58a131ab8cefb1d8b889c5bb22bcfd171
Sergey Kuzminich <SergeyKuzminich@yandex.ru>
2020-07-07 17:11:53 +0300

    Check if http status is standard

--- src/Objects/Error.php
+++ src/Objects/Error.php
@@ -33,30 +33,45 @@ class Error implements ExportPreparationInterface
     /** @var Links */
     public Links $links;
 
+    /** @var array */
+    private const HTTP_STATUSES = [
+        100, 101, 102,
+        200, 201, 202, 203, 204, 205, 206, 207, 208, 226,
+        300, 301, 302, 303, 304, 305, 307, 308,
+        401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 421, 422, 423, 424, 426, 428, 429, 431, 444, 451, 499,
+        500, 501, 502, 503, 504, 505, 506, 507, 508, 509, 510, 511, 599
+    ];
+
     public function __construct(
         ?Exception $e=null,
-        ?string $status=null,
+        ?string $httpStatusCode=null,
         ?string $detail=null,
         ?string $id=null,
-        ?string $code=null,
+        ?string $errorUniqueCode=null,
         ?string $title=null)
     {
         $this->status = null;
         $this->detail = null;
 
         if ($e !== null){
-            $this->status = (string)$e->getCode();
+            if (in_array((int)$e->getCode(), self::HTTP_STATUSES, true)){
+                $this->status = (string)$e->getCode();
+            } else {
+                $this->status = 500;
+                $this->code = (string)$e->getCode();
+            }
+
             $this->detail = $e->getMessage();
         }
-        if ($status !== null) {
-            $this->status = $status;
+        if ($httpStatusCode !== null) {
+            $this->status = $httpStatusCode;
         }
         if ($detail !== null){
             $this->detail = $detail;
         }
         $this->id = $id;
         $this->title = $title;
-        $this->code = $code;
+        $this->code = $errorUniqueCode;
 
         $this->meta = new Meta();
         $this->links = new Links();
