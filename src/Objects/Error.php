73307d36fc62f5b10202cbfdeb10d4f3f9047796
Sergey Kuzminich <SergeyKuzminich@yandex.ru>
2020-07-07 19:17:10 +0300

    Check for getHttpStatusCode in exception method while generating an error response

--- src/Objects/Error.php
+++ src/Objects/Error.php
@@ -33,15 +33,6 @@ class Error implements ExportPreparationInterface
     /** @var Links */
     public Links $links;
 
-    /** @var array */
-    private const HTTP_STATUSES = [
-        100, 101, 102,
-        200, 201, 202, 203, 204, 205, 206, 207, 208, 226,
-        300, 301, 302, 303, 304, 305, 307, 308,
-        401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 421, 422, 423, 424, 426, 428, 429, 431, 444, 451, 499,
-        500, 501, 502, 503, 504, 505, 506, 507, 508, 509, 510, 511, 599
-    ];
-
     public function __construct(
         ?Exception $e=null,
         ?string $httpStatusCode=null,
@@ -54,8 +45,9 @@ class Error implements ExportPreparationInterface
         $this->detail = null;
 
         if ($e !== null){
-            if (in_array((int)$e->getCode(), self::HTTP_STATUSES, true)){
-                $this->status = (string)$e->getCode();
+            if (method_exists($e, 'getHttpStatusCode')) {
+                $this->status = $e->getHttpStatusCode();
+                $this->code = (string)$e->getCode();
             } else {
                 $this->status = 500;
                 $this->code = (string)$e->getCode();
